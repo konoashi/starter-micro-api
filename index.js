913353fb8b2f51c1d029f797b10304d606e2fca5
Muradore Guilhem <64316846+konoashi@users.noreply.github.com>
2023-05-14 18:55:04 +0200

    debug

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+
+const uri = process.env.MONGO_CONNECTION_STRING;
+const client = new MongoClient(uri);
+
+app.post("/api/:anime/:episode/:timestamp", async (req, res) => {
+    let anime = req.params.anime;
+    let episode = req.params.episode;
+    let timestamp = req.params.timestamp;
+    console.log(anime);
+    /*let item = await client.db("my_db")
+                .collection("my_collection")
+                .findOne({my_item: my_item})
+
+    return res.json(item)*/
+})
+
+client.connect(err => {
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+});
